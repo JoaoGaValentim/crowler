e5bf180d672f24401024ddcc1cbb8d304296b835
JoaoGaValentim <joao.gabriel.vale.theodoro@gmail.com>
2026-04-25 16:12:17 -0300

    added messages

--- src/main/java/com/crawler/Main.java
+++ src/main/java/com/crawler/Main.java
@@ -7,7 +7,6 @@ import javax.swing.JOptionPane;
 import com.crawler.exceptions.DocumentNotFoundException;
 import com.crawler.exceptions.ElementNotFoundException;
 import com.crawler.services.CurrencyService;
-// import com.crawler.services.CurrencyService;
 import com.crawler.services.WeatherService;
 
 public class Main {
@@ -29,16 +28,23 @@ public class Main {
                                                                 + weatherService.getMinTemperatureText()
                                                                 + "\nMAX: " + weatherService.getMaxTemperatureText());
                         } else {
-                                JOptionPane.showMessageDialog(null, "Opção inválida!");
+                                JOptionPane.showMessageDialog(null, "Entrada inválida. ", "ALERTA",
+                                                JOptionPane.WARNING_MESSAGE, null);
                         }
 
                 } catch (DocumentNotFoundException docException) {
+                        JOptionPane.showMessageDialog(null, "Documento não pode ser encontrado. ", "ALERTA",
+                                        JOptionPane.WARNING_MESSAGE, null);
                         System.out.println("Documento não pode ser encontrado. ");
                         System.out.println("Erro: " + docException.getMessage());
                 } catch (ElementNotFoundException elementException) {
+                        JOptionPane.showMessageDialog(null, "Elemento não pode ser encontrado. ", "ALERTA",
+                                        JOptionPane.WARNING_MESSAGE, null);
                         System.out.println("Elemento não pode ser encontrado. ");
                         System.out.println("Erro: " + elementException.getMessage());
                 } catch (IOException exception) {
+                        JOptionPane.showMessageDialog(null, "Erro: " + exception.getMessage(), "ERRO",
+                                        JOptionPane.ERROR_MESSAGE, null);
                         System.out.println("Erro: " + exception.getMessage());
                 }
         }
